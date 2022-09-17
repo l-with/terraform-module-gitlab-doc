da60694a76660b6e0cc9fc9ff542a11939559674
Lars With <lars@with.de>
2022-09-17 16:47:50 +0200

    use Tabulator to render table

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
+locals {
+  users_template_vars = merge(tomap({
+    users = data.gitlab_users.users.users
+  }), tomap({
+    tabulator_theme = var.tabulator_theme,
+    html_font_family = var.html_font_family
+  }))
+}
+
 output "users_html" {
-  value = templatefile(local.users_html_template_file, tomap({ users = data.gitlab_users.users.users }))
+  value = templatefile(local.users_html_template_file, local.users_template_vars)
 }
