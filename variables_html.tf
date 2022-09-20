f065c14baf898badb282ea6f702813d841a65a4a
Lars With <lars@with.de>
2022-09-20 10:59:32 +0200

    add template subfolder

--- variables_html.tf
+++ variables_html.tf
@@ -1,7 +1,13 @@
+variable "template_type" {
+  description = "the template type selecting the template subfolder (only 'tabulator' by now)"
+  type        = string
+  default     = "tabulator"
+}
+
 variable "tabulator_theme" {
   description = "the tabulator theme"
   type        = string
-  default     = "modern"
+  default     = "semantic-ui"
 }
 
 variable "html_font_family" {
