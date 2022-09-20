a9320e0114c0d36133960182495ca74c6fa2fa04
Lars With <lars@with.de>
2022-09-20 10:58:55 +0200

    add template subfolder

--- users.tf
+++ users.tf
@@ -1,7 +1,7 @@
 locals {
   users_html_template_file_basename = "users.html.tftpl"
   input_users_html_template_file    = "${var.template_files_path}/${local.users_html_template_file_basename}"
-  default_users_html_template_file  = "${path.module}/templates/${local.users_html_template_file_basename}"
+  default_users_html_template_file  = "${path.module}/templates/${var.template_type}/${local.users_html_template_file_basename}"
   users_html_template_file          = fileexists(local.input_users_html_template_file) ? local.input_users_html_template_file : local.default_users_html_template_file
 }
 
